a754eff670fe87f4ec6a7fc3622ffef44a60d1ad
Rich Friedel <rich.friedel@gmail.com>
2024-03-02 00:29:02 -0500

    Just get things working for now

--- app/src/main/java/net/rf43/composecinema/MainActivity.kt
+++ app/src/main/java/net/rf43/composecinema/MainActivity.kt
@@ -3,6 +3,8 @@ package net.rf43.composecinema
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -22,25 +24,12 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Top) {
+                        CinemaVideoPlayer()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeCinemaTheme {
-        Greeting("Android")
     }
 }
