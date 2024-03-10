c4a3aef4f095e12c39a932b89cf4029afb9e094a
Rich Friedel <rich.friedel@gmail.com>
2024-03-10 00:33:03 -0500

    Add placeholder

--- app/src/main/java/net/rf43/composecinema/home/TopSection.kt
+++ app/src/main/java/net/rf43/composecinema/home/TopSection.kt
@@ -1,12 +1,13 @@
 package net.rf43.composecinema.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,10 +15,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import net.rf43.composecinema.R
+import net.rf43.composecinema.ui.theme.ComposeCinemaTheme
 import net.rf43.composecinema.ui.theme.posterBackgroundGradientEnd
 import net.rf43.composecinema.ui.theme.posterBackgroundGradientStart
 
@@ -27,7 +32,6 @@ internal fun TopSection(
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth()
             .background(
                 Brush.verticalGradient(
                     colors = listOf(
@@ -37,15 +41,17 @@ internal fun TopSection(
                     endY = 400.dp.value
                 )
             ),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Spacer(modifier = Modifier.height(24.dp))
         AsyncImage(
-            modifier = Modifier.height(225.dp),
+            modifier = Modifier.weight(3f),
             model = ImageRequest.Builder(LocalContext.current)
                 .data("https://image.tmdb.org/t/p/w1280/jCWOkfMLsT2sGHadCkmR65MWtJu.jpg")
                 .crossfade(true)
                 .build(),
+            placeholder = painterResource(id = R.drawable.video_chat),
             contentScale = ContentScale.Fit,
             contentDescription = null
         )
@@ -62,7 +68,20 @@ internal fun TopSection(
             maxLines = 3,
             style = MaterialTheme.typography.labelSmall,
             textAlign = TextAlign.Center,
-            modifier = Modifier.padding(horizontal = 20.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 20.dp),
         )
+    }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    ComposeCinemaTheme {
+        // A surface container using the 'background' color from the theme
+        Surface {
+            TopSection()
+        }
     }
 }
