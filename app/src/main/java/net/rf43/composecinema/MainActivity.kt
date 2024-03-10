dd82c5c0f1c5c9c57973644d8416badc076b7ce8
Rich Friedel <rich.friedel@gmail.com>
2024-03-10 14:40:55 -0400

    Lock orientation to portrait

--- app/src/main/java/net/rf43/composecinema/MainActivity.kt
+++ app/src/main/java/net/rf43/composecinema/MainActivity.kt
@@ -1,5 +1,7 @@
 package net.rf43.composecinema
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,11 +9,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.compose.rememberNavController
 import net.rf43.composecinema.navigation.NavigationGraph
 import net.rf43.composecinema.ui.theme.ComposeCinemaTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +25,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val context = LocalContext.current
+                    val activity = context as ComponentActivity
+                    activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
                     val navController = rememberNavController()
                     NavigationGraph(navController = navController)
                 }
